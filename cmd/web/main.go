c6169df63bf6a44ebe8815f4fddb737df5d9ff50
William Tran <wtran29@gmail.com>
2023-07-06 18:01:23 -0700

    include go routine to listen for the websocket channel

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/wtran29/go-websockets/internal/handlers"
 )
 
 func main() {
 	mux := routes()
+
+	log.Println("Starting channel listener")
+	go handlers.ListenForWSChannel()
 
 	log.Println("Starting web server on port 8080")
 
